d8dfdd41aa7082350160889a2c9acfc93c14abf2
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2022-12-30 10:16:35 +0600

    web socket connected

---
+++ controllers/socket.js
@@ -0,0 +1,11 @@
+exports.connected = async () => {
+  console.log("User Connected");
+};
+
+exports.disconnected = async () => {
+  console.log("User Disconnected");
+};
+
+exports.sendConnectedUsers = async () => {
+  console.log("5 Users");
+};
--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const app = express();
 // const server = http.createServer(app);
 // const { Server } = require("socket.io");
 // const io = new Server(server);
-// const io = require("socket.io")(3000, {
-//   cors: { origin: "*" },
-// });
+
+const io = require("socket.io")(process.env.SOCKET_PORT, {
+  cors: { origin: "*" },
+});
 
 const authRoute = require("./routes/auth");
 const textsRoute = require("./routes/texts");
@@ -25,20 +26,20 @@ app.use(
 app.use("/auth", authRoute);
 app.use("/text", textsRoute);
 
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
+const socketController = require("./routes/socket");
 
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected");
-//   });
-// });
+const onConnection = (socket) => {
+  socketController(io, socket);
+};
+
+io.on("connection", onConnection);
 
 mongoose
   .connect(
     `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@127.0.0.1:27017/${process.env.DB_NAME}?authSource=${process.env.AUTH_SOURCE}`
   )
   .then(() => {
-    const port = process.env.PORT || 4000;
+    const port = process.env.PORT;
     app.listen(port, () => console.log(`Server is running at port:${port}`));
     console.log("Connected to mongoDB");
   })
